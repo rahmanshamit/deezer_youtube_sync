ec9fe110428ef172f16aa370c730a161a94e8d8d
Shamit Rahman <shamitrahman@Shamits-MacBook-Pro.local>
2024-02-09 01:39:46 -0800

    parse out first track for testing

--- playlist.py
+++ playlist.py
@@ -1,4 +1,8 @@
 import requests
 
 response = requests.get("https://api.deezer.com/playlist/9974949442/tracks")
-print(response.json())
+tracklist_json = response.json()
+tracklist_json = tracklist_json["data"]
+
+# gets the first item
+print(tracklist_json[0])
